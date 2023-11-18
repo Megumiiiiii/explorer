dce02de69672559bf94edc2cb28a8d410f0c5228
Megumiiiiii <megumii6976@gmail.com>
2023-11-19 05:47:30 +0700

    Change Icon Color

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -218,7 +218,7 @@ function selected(route: any, nav: NavLink) {
           <RouterLink to="/wallet/suggest"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
           >
-            <Icon icon="mdi:wallet-bifold-outlines" class="text-xl mr-2" />
+            <Icon icon="mdi:controller-classic" class="text-xl text-red-400 mr-2" />
             <div
               class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
             >
@@ -232,7 +232,7 @@ function selected(route: any, nav: NavLink) {
           target="_blank"
           class="py-2 px-4 flex items-center cursor-pointer rounded-lg hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:twitter" class="text-xl mr-2" />
+          <Icon icon="mdi:twitter" class="text-xl text-cyan-500 mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
@@ -244,7 +244,7 @@ function selected(route: any, nav: NavLink) {
           target="_blank"
           class="py-2 px-4 flex items-center rounded-lg cursor-pointer hover:bg-gray-100 dark:hover:bg-[#373f59]"
         >
-          <Icon icon="mdi:google-downasaur" class="text-xl mr-2" />
+          <Icon icon="mdi:google-downasaur" class="text-xl text-green-300 mr-2" />
           <div
             class="text-base capitalize flex-1 text-gray-600 dark:text-gray-200"
           >
